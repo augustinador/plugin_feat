9263a933f3ef3d83cb4a34b6b3e040c951be96f9
admin <admin>
2021-01-26 21:33:25 +0100

    Edited files

--- custom-recipes/fuzzy/recipe.py
+++ custom-recipes/fuzzy/recipe.py
@@ -1,32 +1,14 @@
-# Code for custom code recipe fuzzy (imported from a Python recipe)
 
-# To finish creating your custom recipe from your original PySpark recipe, you need to:
-#  - Declare the input and output roles in recipe.json
-#  - Replace the dataset names by roles access in your code
-#  - Declare, if any, the params of your custom recipe in recipe.json
-#  - Replace the hardcoded params values by acccess to the configuration map
 
-# See sample code below for how to do that.
-# The code of your original recipe is included afterwards for convenience.
-# Please also see the "recipe.json" file for more information.
-
-# import the classes for accessing DSS objects from the recipe
 import dataiku
-# Import the helpers for custom recipes
 from dataiku.customrecipe import *
 
-# Inputs and outputs are defined by roles. In the recipe's I/O tab, the user can associate one
-# or more dataset to each input and output role.
-# Roles need to be defined in recipe.json, in the inputRoles and outputRoles fields.
 
-# To  retrieve the datasets of an input role named 'input_A' as an array of dataset names:
-input_A_names = get_input_names_for_role('input_A_role')
-# The dataset objects themselves can then be created like this:
-input_A_datasets = [dataiku.Dataset(name) for name in input_A_names]
+input_A_names = get_input_names_for_role('input_A_role')[0]
+
 
 # For outputs, the process is the same:
-output_A_names = get_output_names_for_role('main_output')
-output_A_datasets = [dataiku.Dataset(name) for name in output_A_names]
+output_A_names = get_output_names_for_role('main_output')[0]
 
 
 # The configuration consists of the parameters set up by the user in the recipe Settings tab.
